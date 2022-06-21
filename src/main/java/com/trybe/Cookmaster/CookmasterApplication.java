cd1e4ed964932a3a96be0eadb8ca64531b3d0d58
Rodrigo Luiz Rech <rodrigorech92@gmail.com>
2022-06-21 11:49:17 -0300

    Initial Commit

---
+++ src/main/java/com/trybe/Cookmaster/CookmasterApplication.java
@@ -0,0 +1,13 @@
+package com.trybe.Cookmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trybe/Cookmaster/CookmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trybe.Cookmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
